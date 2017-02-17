2e3b17e8e70d397ba75a814a1d2962b67a23241e
badeleux <badeleux@gmail.com>
2017-02-17 18:18:30 +0100

    Created Kit and UI Frameworks

---
+++ CurrencyTickerKit/CurrencyTickerKit.h
@@ -0,0 +1,19 @@
+//
+//  CurrencyTickerKit.h
+//  CurrencyTickerKit
+//
+//  Created by Kamil Badyla on 17.02.2017.
+//  Copyright © 2017 ScienceSpir.IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CurrencyTickerKit.
+FOUNDATION_EXPORT double CurrencyTickerKitVersionNumber;
+
+//! Project version string for CurrencyTickerKit.
+FOUNDATION_EXPORT const unsigned char CurrencyTickerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CurrencyTickerKit/PublicHeader.h>
+
+
---
+++ CurrencyTickeriOS/CurrencyTickeriOS.h
@@ -0,0 +1,19 @@
+//
+//  CurrencyTickeriOS.h
+//  CurrencyTickeriOS
+//
+//  Created by Kamil Badyla on 17.02.2017.
+//  Copyright © 2017 ScienceSpir.IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CurrencyTickeriOS.
+FOUNDATION_EXPORT double CurrencyTickeriOSVersionNumber;
+
+//! Project version string for CurrencyTickeriOS.
+FOUNDATION_EXPORT const unsigned char CurrencyTickeriOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CurrencyTickeriOS/PublicHeader.h>
+
+
